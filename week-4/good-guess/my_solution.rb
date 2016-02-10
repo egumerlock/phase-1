053b37d71c6b58c70de730b03dbaa9f24bb203c4
Leo Kukhar <leokukhar@gmail.com>
2016-02-09 19:56:24 -0500

    Add Exercise 4.5.1 - 4.5.5

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,14 @@
+# Good Guess
+
+# I worked on this challenge Kevin Niu.
+
+
+# Your Solution Below
+
+def good_guess?(integer)
+  if integer == 42
+    return true
+  else
+    return false
+  end
+end
