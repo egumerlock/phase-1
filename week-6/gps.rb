5526cd106f14d843089007691ef0f1ade99240a2
Leo Kukhar <leokukhar@gmail.com>
2016-02-23 12:02:38 -0500

    Add GPS 2.3

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,38 +1,36 @@
 # Your Names
-# 1)
-# 2)
-
-# We spent [#] hours on this challenge.
+# 1)Leo Kukhar
+# 2)Eric Gumerlock
+# We spent [1] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
+#Define the method, two parameters, an item and a number of ingredients
 def serving_size_calc(item_to_make, num_of_ingredients)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+  recipe = {"cookie" => 1, "cake" =>  5, "pie" => 7}
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
-    end
-  end
-
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
-
-  serving_size = library.values_at(item_to_make)[0]
-  remaining_ingredients = num_of_ingredients % serving_size
-
+#If it is something we cannot make, raise argument
+  raise ArgumentError.new("#{item_to_make} is not a valid input") unless recipe.has_key?(item_to_make)
+#We set serving size to value at key
+  amount_needed = recipe[item_to_make]
+  remaining_ingredients = num_of_ingredients % amount_needed
+  food_amount = num_of_ingredients / amount_needed
+#Specifies two cases, no ingredients leftover or you have leftover ingredients
   case remaining_ingredients
   when 0
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
+    return "Calculations complete: Make #{food_amount} of #{item_to_make}"
+  when (1..5)
+    return "Calculations complete: Make #{food_amount} of #{item_to_make}, you can also make #{remaining_ingredients} cookie."
+  when 6
+    return "Calculations complete: Make #{food_amount} of #{item_to_make}, you can also make 1 cake or #{remaining_ingredients} cookie."
   else
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    return "Calculations complete: Make #{food_amount} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
   end
 end
-
+#call serving_size method
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
+p serving_size_calc("pie", 13)
 p serving_size_calc("cake", 5)
 p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
@@ -40,3 +38,17 @@ p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
+
+=begin
+What did you learn about making code readable by working on this challenge? -I learned how the proper way to use an unless statement. I also learn that there are a multitude of ways on going about things, some of which are a lot shorter than others.
+
+Did you learn any new methods? What did you learn about them?
+- I leard the method #has_key? for the hash, it'll return trunt if the specified key that is being looked for is present in the hash.
+
+What did you learn about accessing data in hashes?
+-The you don't have to iterate over each key value pair in a hash to see if a key or value is present. There is a built it method that will scan the whole hash for you.
+
+What concepts were solidified when working through this challenge?
+-Refactoring was solidified for me, as well as making your code more dry.
+
+=end
