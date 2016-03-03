edeb3691f7cf185d6f997e7b9dc11bf91f9c1ad7
Leo Kukhar <leokukhar@gmail.com>
2016-03-03 14:57:20 -0500

    Add Game Code

---
+++ week-7/game.js
@@ -0,0 +1,100 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description: The character is stuck in a room with no light, using his or her memory, he or she must reach the exit, there are monsters in the dark. If you step into the same spot as them its game over.
+// Overall mission: Escape the room
+// Goals: Use memory to find the exit.
+// Characters: The main character, monsters.
+// Objects: Main character, Monsters, door
+// Functions: moveUp, moveDown, moveLeft, moveRight
+
+// Pseudocode
+// Create a player object with initial position of X: 0 and Y: 0
+// Give player methods to move up, down, left, right.
+// Create a door object with a position of X: 30 and Y: 25
+// Create a monster1 object with a position of X: -10 and Y: 5
+// Create a monster2 object with a position of X: 5 and Y: -10
+// Move player, if spot is open, tell player where he or she is. If monster on same spot, game over. If door is on same spot, you win.
+//
+
+// Initial Code
+console.log("You wake up in a dark room and you cannot see a thing. You hear monster noises. You need to find your way out, without meeting the terrible monsters. Remember how many steps you take, incase you have to do this again.");
+
+var door = {
+  posX: 30,
+  posY: 25
+};
+
+var monster1 = {
+  posX: -10,
+  posY: -5
+};
+
+var monster2 = {
+  posX: -5,
+  posY: -10
+};
+
+var player = {
+  posX: 0,
+  posY: 0,
+
+  move: function (direction) {
+    if (direction === "up") {
+      player.posY += 5;}
+
+    else if (direction === "down") {
+      player.posY -= 5;}
+
+    else if (direction === "left") {
+      player.posX -= 5;}
+
+    else if (direction === "right") {
+      player.posX += 5;}
+
+  if (player.posX === monster1.posX && player.posY === monster1.posY){
+    console.log("One of the monsters got you in the dark!!");
+  }
+  else if (player.posX === monster2.posX && player.posY === monster2.posY){
+    console.log("One of the monsters got you in the dark!!");
+  }
+  else if (player.posX === door.posX && player.posY === door.posY){
+    console.log("You reached the door. You open it and bright light fills the once dark room, killing all the monsters inside.");
+  }
+  else {
+    console.log("You're safe for now, you can't see anything. Do you remember how many steps you took?");
+    }
+  }
+};
+
+player.move("right")
+player.move("right")
+player.move("right")
+player.move("right")
+player.move("right")
+player.move("right")
+player.move("up")
+player.move("up")
+player.move("up")
+player.move("up")
+player.move("up")
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
