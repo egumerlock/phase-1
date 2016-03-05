869b49644205f9493bba9745a31b78cb30142b49
Leo Kukhar <leokukhar@gmail.com>
2016-03-04 22:27:22 -0500

    Refactor

--- week-7/group_project_solution.js
+++ week-7/group_project_solution.js
@@ -27,3 +27,101 @@
 // 9. Call the sort method on the groups so that the numbers are sorted from least to greatest.
 // 10. Using an if/else statement, if the length of the group is an odd number, return the number which is index[length/2]
 // 11. Else return the average of the two middle numbers, (index[length / 2] + index[lenght / 2 + 1]) / 2
+
+// Person 3 (Sean Norton)
+
+//  Release 3:
+
+
+function sum(array){
+  var total = 0;
+  for (var i = 0; i < array.length; i++){
+    total += array[i];
+  }
+  return total;
+};
+
+function mean(array){
+  var avg = sum(array)/array.length;
+  return avg;
+};
+
+function median(array){
+  var count = array.length - 1;
+  var sorted = array.sort();
+  var half = Math.floor(count/2);
+  (array.length % 2 !== 0) ? array[half] : ((array[half] + array[half + 1]) / 2)
+};
+
+
+// TEST CODE
+
+oddLengthArray  = [1, 2, 3, 4, 5, 5, 7]
+evenLengthArray = [4, 4, 5, 5, 6, 6, 6, 7]
+
+
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+// tests for sum
+assert(
+  (sum instanceof Function),
+  "sum should be a Function.",
+  "1. "
+)
+
+assert(
+  sum(oddLengthArray) === 27,
+  "sum should return the sum of all elements in an array with an odd length.",
+  "2. "
+)
+
+assert(
+  sum(evenLengthArray) === 43,
+  "sum should return the sum of all elements in an array with an even length.",
+  "3. "
+)
+
+// tests for mean
+assert(
+  (mean instanceof Function),
+  "mean should be a Function.",
+  "4. "
+)
+
+assert(
+  mean(oddLengthArray) === 3.857142857142857,
+  "mean should return the average of all elements in an array with an odd length.",
+  "5. "
+)
+
+assert(
+  mean(evenLengthArray) === 5.375,
+  "mean should return the average of all elements in an array with an even length.",
+  "6. "
+)
+
+// tests for median
+assert(
+  (median instanceof Function),
+  "median should be a Function.",
+  "7. "
+)
+
+assert(
+  median(oddLengthArray) === 4,
+  "median should return the median value of all elements in an array with an odd length.",
+  "8. "
+)
+
+assert(
+  median(evenLengthArray) === 5.5,
+  "median should return the median value of all elements in an array with an even length.",
+  "9. "
+)
